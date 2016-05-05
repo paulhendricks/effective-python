aa493f8a1fbe1a60e75008b1965b51dd3979d14b
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-05-05 14:34:27 -0400

    added example

--- ch05/item-41.py
+++ ch05/item-41.py
@@ -1,9 +1,50 @@
 #!/usr/bin/python3
-"""TO BE EDITED
+"""Consider concurrent.futures for True Parallelism
 
-Not Complete!
+Complete!
 """
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+import time
+
+
+numbers = [(1963309, 2265973), (2030677, 3814172),
+           (1551645, 2229620), (2039045, 2020802),
+           (1551645, 2229620), (2039045, 2020802),
+           (1551645, 2229620), (2039045, 2020802)]
+
+
+def gcd(pair):
+    a, b = pair
+    low = min(a, b)
+    for i in range(low, 0, -1):
+        if a % i == 0 and b % i == 0:
+            return i
+
+
+def main_1():
+    start = time.time()
+    results = list(map(gcd, numbers))
+    end = time.time()
+    return 'Took %.3f seconds' % (end - start)
+
+
+def main_2():
+    start = time.time()
+    pool = ThreadPoolExecutor(max_workers=8)
+    results = list(pool.map(gcd, numbers))
+    end = time.time()
+    return 'Took %.3f seconds' % (end - start)
+
+
+def main_3():
+    start = time.time()
+    pool = ProcessPoolExecutor(max_workers=8)
+    results = list(pool.map(gcd, numbers))
+    end = time.time()
+    return 'Took %.3f seconds' % (end - start)
 
 
 if __name__ == '__main__':
-    print("Hello world!")
+    print(main_1())
+    print(main_2())
+    print(main_3())
